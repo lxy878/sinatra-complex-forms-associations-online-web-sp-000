dc94ebdacfdee10faedc53cf680e6b3ed7e4f2b0
Xiaoyi Liu <lxy878@gmail.com>
2020-05-11 14:03:30 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -10,7 +10,7 @@ class OwnersController < ApplicationController
     erb :'/owners/new'
   end
 
-  post '/owners' d
+  post '/owners' do
     @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
